6c55017e1f63b102f17f31ff882927936d61a3cd
Ravindrakumara <44864848+Ravindrakumara@users.noreply.github.com>
2020-08-12 17:15:22 +0530

    Update settings.py
    Static file setting

--- eshop/settings.py
+++ eshop/settings.py
@@ -130,14 +130,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 #---------------------------------------------
-MEDIA_URL = '/pics/'
+MEDIA_URL = "/pics/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "pics")
 #---------------------------------------------
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Auth---------------------------
 
 AUTHENTICATION_BACKENDS = (
